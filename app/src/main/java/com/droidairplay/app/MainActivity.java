785fff95cb395cc668b85e90e45ea6b26cbfbcb2
yulong li <m@liyl.me>
2015-09-01 20:08:45 +0800

    finish project strucher

---
+++ app/src/main/java/com/droidairplay/app/MainActivity.java
@@ -0,0 +1,67 @@
+package com.droidairplay.app;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+import droid.airplay.AirPlayServer;
+
+
+public class MainActivity extends ActionBarActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings)
+        {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    boolean isOnAir = false;
+    public void startAirPlay(View view)
+    {
+        String msg = "bug occur!";
+        if(isOnAir && AirPlayServer.getIstance().isOn)
+        {
+            msg = "airplay already on!";
+        }
+        else
+        {
+            final AirPlayServer airPlayServer = AirPlayServer.getIstance();
+            airPlayServer.setRtspPort(8998);
+            Thread airThread = new Thread(airPlayServer);
+            airThread.start();
+            //airPlayServer.run();
+            msg = "starting...";
+            isOnAir = true;
+        }
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+}
--- oldairplay/README.md
+++
@@ -1,2 +0,0 @@
-# airplay-server-on-android-phone
-app based on DroidAirPlay :https://github.com/pentateu/DroidAirPlay
